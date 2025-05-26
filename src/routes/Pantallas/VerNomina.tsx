cb31d0e7bccf95a6bf1ed872ed053a84fe4fd37d
polillads11 <polilladasilva0@gmail.com>
2025-05-26 14:24:43 -0300

    Ver nomina completado

--- src/routes/Pantallas/VerNomina.tsx
+++ src/routes/Pantallas/VerNomina.tsx
@@ -1,3 +1,5 @@
+import { div } from "framer-motion/client";
+
 const recibo = {
   empresa: "Shain Flow",
   periodo: "Enero 2025",
@@ -13,9 +15,9 @@ const recibo = {
     { tipo: "IESS", monto: 500 },
     { tipo: "Impuestos", monto: 1000 },
   ],
-  total_ingresos: 12000,
-  total_deducciones: 1500,
-  total_neto: 10500,
+  total_ingresos: '1.200.000',
+  total_deducciones: '150.000',
+  total_neto: '1.050.000',
 };
 
 function CabeceraRecibo() {
@@ -56,20 +58,90 @@ function ListaItems() {
 
 export function VerNomina() {
     return (
-        <div>
-            <CabeceraRecibo />
-            <section>
-                <h2>Datos del Empleado</h2>
-                <p>Nombre: {recibo.empleado.nombre}</p>
-                <p>Número de Empleado: {recibo.empleado.numero_empleado}</p>
-            </section>
-            <ListaItems  />
-            <section>
-                <h2>Totales</h2>
-                <p>Total Ingresos: {recibo.total_ingresos}</p>
-                <p>Total Deducciones: {recibo.total_deducciones}</p>
-                <p>Total Neto: {recibo.total_neto}</p>
-            </section>
-        </div>
+      <div style={{display: 'flex', justifyContent: 'center', position: 'relative'}}>
+      <div style={{position: 'absolute', border: '1px solid black', backgroundColor: '#fff', borderRadius: '5px', boxShadow: '0 0 20px rgba(0, 0, 0, 0.1)', width: '70%' }}>
+        <section style={{ border: '1px solid black', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', marginTop: '20px', marginLeft: '10px', marginRight: '10px', marginBottom: '10px' }}>
+          <CabeceraRecibo />
+        </section>
+        <section style={{ border: '1px solid black', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', marginTop: '20px', marginLeft: '10px', marginRight: '10px', marginBottom: '10px' }}>
+          <h2>Datos del Empleado</h2>
+          <p>Nombre: {recibo.empleado.nombre}</p>
+          <p>Número de Empleado: {recibo.empleado.numero_empleado}</p>
+        </section>
+        <section style={{ border: '1px solid black', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', marginTop: '20px', marginLeft: '10px', marginRight: '10px', marginBottom: '10px' }}>
+          <table>
+            <thead>
+              <tr>
+                <th>Concepto</th>
+                <th style={{width:"80%"}}>Decripción</th>
+                <th>Ingreso</th>
+                <th>Descuento</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>01</td>
+                <td>Haber Mensual</td>
+                <td>600.000</td>
+                <td>62.000</td>
+              </tr>
+              <tr>
+                <td>02</td>
+                <td>Antigüedad</td>
+                <td>200.000</td>
+                <td>24.000</td>
+              </tr>
+              <tr>
+                <td>03</td>
+                <td>Presentismo</td>
+                <td>70.000</td>
+              </tr>
+              <tr>
+                <td>04</td>
+                <td>Horas extras</td>
+                <td>140.000</td>
+              </tr>
+              <tr>
+                <td>05</td>
+                <td>Descuento Obra Social</td>
+                <td></td>
+                <td>65.000</td>
+              </tr>
+              <tr>
+                <td>04</td>
+                <td>Descuento ANSSAL</td>
+                <td></td>
+                <td>28.000</td>
+              </tr>
+              <tr>
+                <td>04</td>
+                <td>Descuento Ley 19.032</td>
+                <td></td>
+                <td>13.000</td>
+              </tr>
+              <tr>
+                <td>04</td>
+                <td>Impuesto a las Ganancias</td>
+                <td></td>
+                <td>5.000</td>
+              </tr>
+              <tr>
+                <td>04</td>
+                <td>Descuento Sindical</td>
+                <td></td>
+                <td>28.000</td>
+              </tr>
+            </tbody>
+          </table>
+          {/*<ListaItems  />*/}
+        </section>
+        <section style={{ border: '1px solid black', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', marginTop: '20px', marginLeft: '10px', marginRight: '10px', marginBottom: '10px' }}>
+          <h2>Totales</h2>
+          <p>Total Ingresos: {recibo.total_ingresos}</p>
+          <p>Total Deducciones: {recibo.total_deducciones}</p>
+          <p>Total Neto: {recibo.total_neto}</p>
+        </section>
+      </div>
+      </div>
     )
 }
