330f3c8a5fbf1e1957feb2e377bea7ad7ee19177
Lautaro Moreno <lemoreno2002@gmail.com>
2025-05-30 19:29:29 -0300

    Primeros estilos del default-layout

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -14,19 +14,23 @@ export default function DefaultLayout({ children }: DefaultLayoutProps) {
         <div className="empresa-descripcion">
           <h2>Sobre la empresa</h2>
           <p>
-            Somos una empresa tecnológica enfocada en brindar soluciones innovadoras
-            para la gestión de recursos humanos, nóminas y asistencia. Nos destacamos
-            por nuestra eficiencia, compromiso y adaptabilidad al cambio.
+          “Soluciones de RRHH con visión de futuro”  
           </p>
+<p>"Mirai" (未来) significa futuro en japonés, lo cual le da un toque tecnológico, moderno y con visión a largo plazo.</p> 
+<p>"Solutions" refuerza que no solo desarrollan productos, sino que brindan soluciones concretas. 
+Producto: SHAIN FLOW “Flujo de empleados” </p>
+<p>"Shain" (社員) significa empleado o miembro de una organización. ● "Flow" le da esa idea de procesos flujos, automatizados, modernos.</p> 
+
         </div>
         <div className="empresa-integrantes">
           <h3>Nuestro equipo</h3>
           <ul>
-            <li>Laura Gómez – CEO</li>
-            <li>Martín Pérez – CTO</li>
-            <li>Sofía Ramírez – Analista Funcional</li>
-            <li>Juan Ortega – Desarrollador Frontend</li>
-            <li>Camila Díaz – RRHH</li>
+            <li>Cristian Jurajuria – Scrum Master / DBA</li>
+            <li>Lautaro Moreno – Desarrollador Frontend</li>
+            <li>Pablo Da Silva - Desarrollador Frontend</li>
+            <li>Abel Aquino - Desarrollador Backend</li>
+            <li>Rodrigo Montoro – Desarrollador Backend / Analista Funcional</li>
+            <li>Mauro Palavecino – Desarrollador Backend / QA Tester</li>
           </ul>
         </div>
       </div>
