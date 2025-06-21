f9db3175240e9fea45e809e8c4daffc60b5e48ba
Cristian Jurajuria <cljurajuria23@gmail.com>
2025-06-20 23:10:04 -0300

    arreglar nombre de formulario pa que ande todo

--- src/components/AgregaSalario.tsx
+++ src/components/AgregaSalario.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CalendarioInput from "./Calendario";
-import "./estilos/ForuSalario.css";
+import "./estilos/FormularioSalario.css";
 
 interface NuevoSalario {
   puesto: string;
