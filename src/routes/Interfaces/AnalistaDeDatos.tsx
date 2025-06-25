86f40d3658e51ea94e8995bba64e666a3b31aa51
LautaroMoreno2002 <lemoreno2002@gmail.com>
2025-06-25 16:35:31 -0300

    Quitar nomina y reporteria de analista

--- src/routes/Interfaces/AnalistaDeDatos.tsx
+++ src/routes/Interfaces/AnalistaDeDatos.tsx
@@ -7,8 +7,8 @@ const navItemsAnalista = [
   { label: "Datos laborales", icon: "fa-solid fa-user-tie", path: "datosLaborales" },
   { label: "Asistencias", icon: "fa-regular fa-id-card", path: "asistencias" },
   { label: "Empleados", icon: "fa-solid fa-users", path: "empleados" },
-  { label: "Reportería", icon: "fa-solid fa-chart-simple", path: "reportes-analista" },
-  { label: "Nóminas", icon: "fa-solid fa-coins", path: "#" },
+  // { label: "Reportería", icon: "fa-solid fa-chart-simple", path: "reportes-analista" },
+  // { label: "Nóminas", icon: "fa-solid fa-coins", path: "#" },
   { label: "Dashboard", icon: "fa-solid fa-chart-bar", path: "dashboard" },
   { label: "Cerrar sesión", icon: "fa-solid fa-lock", path: "/login" },
 ];
